6e3487e1534c5e173941882c0efea08048cc1923
aton-py <h.kalinka@gmail.com>
2021-08-26 10:10:02 -0300

    Add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container, Line } from "./styles";
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+      <Line />
+      <span>Made with ❤ by Kali to Concrete</span>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  span {
+    margin-top: 2rem;
+  }
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  max-width: 1280px;
+  border-top: 1px solid;
+`;
