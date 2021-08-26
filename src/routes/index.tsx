cbd339d41d0de5cb51b06b8e77fc5dcd914904e2
aton-py <h.kalinka@gmail.com>
2021-08-26 10:13:40 -0300

    Add Routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+
+import Profile from "../pages/Profile";
+
+const routes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:username" element={<Profile />} />
+    </Routes>
+  );
+};
+
+export default routes;
