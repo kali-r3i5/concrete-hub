178de2292c4766422462297f7cc1f3d15e005e4c
aton-py <h.kalinka@gmail.com>
2021-08-26 10:12:37 -0300

    Add Loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Loader: React.FC = () => {
+  return (
+    <Container>
+      <span />
+    </Container>
+  );
+};
+
+export default Loader;
