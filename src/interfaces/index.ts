73961bbebf84d43e6e4d34fc95e408818bf1f17f
aton-py <h.kalinka@gmail.com>
2021-08-26 10:10:38 -0300

    Add Interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,47 @@
+export interface APIUserInterface {
+  login: string;
+  username: string;
+  name: string;
+  followers: number;
+  following: number;
+  avatar_url: string;
+  email?: string;
+  bio: string;
+  public_repos: number;
+}
+
+export interface APIRepoInterface {
+  name: string;
+  owner: {
+    login: string;
+  };
+  stargazers_count: number;
+  forks: number;
+  html_url: string;
+  language?: string;
+  description?: string;
+}
+
+export interface ProfileDataInterface {
+  username: string;
+  name: string;
+  followers: number;
+  following: number;
+  avatarUrl: string;
+  email?: string;
+  bio: string;
+  public_repos: number;
+}
+
+export interface RepoCardInterface {
+  username: string;
+  reponame: string;
+  description?: string;
+  stars: number;
+}
+
+export interface ProfileInterface {
+  user?: APIUserInterface;
+  repos?: APIRepoInterface[];
+  error?: string;
+}
